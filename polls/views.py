83b213a24fbca8588fb16a85bbc4e4b3b339725c
Rauan <rauanpires@gmail.com>
2021-09-15 21:57:01 -0300

    Adds views and urls (mock)

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Index da Polls!")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}.")
